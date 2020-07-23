d46714cd090c864169149e3c2cf071ce3f62d964
buikiendp <61977367+buikiendp@users.noreply.github.com>
2020-07-24 00:19:39 +0700

    Update install.sh
    Add multi-threading

--- install.sh
+++ install.sh
@@ -18,7 +18,7 @@ g++ -std=c++11 -o $LDIR/src/genSpecies $LDIR/src/genSpecies.cpp -O3
 g++ -std=c++11 -o $LDIR/src/buildDB  $LDIR/src/compress.cpp -O3
 g++ -std=c++11 -o $LDIR/src/uniqueDB  $LDIR/src/DTB_unique.cpp -O3
 g++ -std=c++11 -o $LDIR/src/translate  $LDIR/src/translate.cpp -O3
-g++ -std=c++11 -o $LDIR/src/classify  $LDIR/src/classify.cpp $LDIR/src/seqreader.cpp -O3
+g++ -fopenmp -std=c++11 -o $LDIR/src/classify  $LDIR/src/classify.cpp $LDIR/src/seqreader.cpp -O3
 
 mv $LDIR/src/buildDB $LDIR/
 mv $LDIR/src/uniqueDB $LDIR/
