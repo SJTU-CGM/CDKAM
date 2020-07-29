6f34b423a903912c7a3f72935c3ee0387bfb2cfa
buikiendp <61977367+buikiendp@users.noreply.github.com>
2020-07-29 10:32:59 +0700

    Update CDKAM.sh

--- CDKAM.sh
+++ CDKAM.sh
@@ -12,13 +12,15 @@ FSCRPT=$(readlink -f "$0")
 LDIR=$(dirname "$FSCRPT")
 
 if [ $# -lt 4 ]; then
-	echo "Usage: $0 <DTB path> <input> <output> <--fasta/--fastq>"
+	echo "Usage: $0 DBname input output --fasta/--fastq"
+	echo "Or"
+	echo "Usage: $0 DBname input output --fasta/--fastq nthread N"
 	exit
 fi
 
 for library_name in "database_Taxo"  "database_Suffix"  "database_Size"
 do
-	if [ ! -e $LDIR/$1/$library_name.txt ]; then
+	if [ ! -e $LDIR/$1/$library_name ]; then
 		echo "Database does not contain necessary file $library_name"
 		exit 0
 	fi
