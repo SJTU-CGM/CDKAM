71d89f45fe2843ebdd0de292f8c10c8f129a24e8
buikiendp <61977367+buikiendp@users.noreply.github.com>
2020-09-16 20:51:09 +0700

    Update build_database.sh

--- build_database.sh
+++ build_database.sh
@@ -13,11 +13,16 @@ LDIR=$(dirname "$FSCRPT")
 
 
 if [ $# -lt 1 ]; then
+	echo "Building the database."
 	echo "Usage: $0 <DTB path> "
 	exit
 fi
 
+if [ -e $LDIR/$1/targets.txt ]; then
+	rm $LDIR/$1/targets.txt
+fi
 touch $LDIR/$1/targets.txt
+
 for library_name in "archaea"  "bacteria"  "viral"  "fungi"  "plant"  "human"  "protozoa"
 do
 	if [ -e $LDIR/$1/$library_name.txt ]; then
@@ -26,7 +31,20 @@ do
 	fi
 done
 
+if [ ! -e $LDIR/$1/taxanomy/nodes.dmp ]; then
+	echo "The taxonomy tree is missing. Please run the command   ./download_taxonomy.sh $1"
+	exit 0
+fi
 
+echo "The first step: collecting kmers."
 $LDIR/buildDB $LDIR/$1/targets.txt $LDIR/$1/nameFamily.txt $LDIR/$1/database_full.txt
+
+if [ ! -e $LDIR/$1/database_full.txt ]; then
+	echo "The first step is failed."
+	exit 0
+fi
+
+echo "The second step: solving kmers collision."
 $LDIR/uniqueDB $LDIR/$1/database_full.txt $LDIR/$1/database
 rm $LDIR/$1/database_full.txt
+echo "Building database: DONE."
